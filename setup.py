d8117cdce237c97c6635614edf0faa9dfd5b001f
vinit <vinit.londhe21@gmail.com>
2022-12-18 14:37:33 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 # Just to create proper packages, libraries so that it can be shared and used for multiple purposes
 
 from setuptools import find_packages,setup
+from typing import List     # The typing module provides us with Type Aliases, which is defined by assigning a type to the alias.
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    
+    return requirement_list
 
 setup(
     name="sensor",
@@ -8,6 +20,6 @@ setup(
     author="vinit",
     author_email="vinit.londhe21@gmail.com",
     packages = find_packages(),
-    install_requires=[]
+    install_requires=get_requirements()  #["pymongo==4.2.0"]
 )
 
