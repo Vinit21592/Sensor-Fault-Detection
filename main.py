e27c3fed7062c633bc66c31c8e28e67d151801a1
vinit <vinit.londhe21@gmail.com>
2022-12-21 01:19:05 +0530

    code updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.configuration.mongo_db_connection import MongoClient
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
+    mongodb_client = MongoClient()
     print("collection name:",mongodb_client.database.list_collection_names())
--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -3,14 +3,14 @@ from sensor.constant.database import DATABASE_NAME
 import certifi
 ca = certifi.where()
 
-class MongoDBClient:
+class MongoClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
-            if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://XXXXXX:XXXXXX@cluster0.dl9uoq9.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoDBClient(mongo_db_url, tlsCAFile=ca)
-            self.client = MongoDBClient.client
+            if MongoClient.client is None:
+                mongo_db_url = "mongodb+srv://Vinit21592:vins21592@cluster0.dl9uoq9.mongodb.net/?retryWrites=true&w=majority"
+                MongoClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
