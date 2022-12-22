805b7a347d358c22b6033c0a7f5296a1dffaed2a
vinit <vinit.londhe21@gmail.com>
2022-12-22 19:57:01 +0530

    Data Ingestion completed

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -11,7 +11,7 @@ class MongoClient:
         try:
             if MongoClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                #mongo_db_url = "mongodb+srv://Vinit21592:vins21592@cluster0.dl9uoq9.mongodb.net/?retryWrites=true&w=majority"
+                # mongo_db_url = "mongodb+srv://Vinit21592:vins21592@cluster0.dl9uoq9.mongodb.net/?retryWrites=true&w=majority"
                 MongoClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoClient.client
             self.database = self.client[database_name]
