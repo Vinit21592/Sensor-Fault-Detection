93c20515b51d40b50f4a84a510d7e3ce66c7e611
vinit <vinit.londhe21@gmail.com>
2022-12-22 19:54:51 +0530

    Data Ingestion completed

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -3,17 +3,22 @@ from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os,sys
+from sensor.components.data_ingestion import DataIngestion
 
 class TrainPipeline:
 
     def __init__(self):
-        training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        self.training_pipeline_config=training_pipeline_config
+        self.training_pipeline_config = TrainingPipelineConfig()
+        
 
     def start_data_ingestion(self)->DataIngestionArtifact: # type: ignore
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e,sys) # type: ignore
 
