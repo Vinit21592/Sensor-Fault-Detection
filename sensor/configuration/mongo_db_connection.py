902b738807a476fc50d5a49bdd3ecccee65ca306
vinit <vinit.londhe21@gmail.com>
2022-12-23 01:49:51 +0530

    code changes

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -3,7 +3,7 @@ from sensor.constant.database import DATABASE_NAME
 from sensor.constant.env_variable import MONGODB_URL_KEY
 from sensor.exception import SensorException
 import certifi
-import os
+import os,sys
 ca = certifi.where()
 
 class MongoClient:
@@ -18,4 +18,4 @@ class MongoClient:
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
-            raise SensorException(e,sys)
+            raise SensorException(e,sys) # type: ignore
