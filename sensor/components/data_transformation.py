cb5b6e78815f65cedd14000437b05d4854fba474
vinit <vinit.londhe21@gmail.com>
2022-12-27 13:11:07 +0530

    Transformation completed

--- sensor/components/data_transformation.py
+++ sensor/components/data_transformation.py
@@ -62,42 +62,50 @@ class DataTransformation:
 
     def initiate_data_transformation(self,) -> DataTransformationArtifact:
         try:
+            
             train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
             test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
             preprocessor = self.get_data_transformer_object()
 
+
             #training dataframe
-            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN],axis=1)
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
             target_feature_train_df = train_df[TARGET_COLUMN]
-            target_feature_train_df = target_feature_train_df.replace(TargetValueMapping().to_dict()) # type: ignore
+            target_feature_train_df = target_feature_train_df.replace(TargetValueMapping().to_dict())
 
             #testing dataframe
-            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN],axis=1)
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
             target_feature_test_df = test_df[TARGET_COLUMN]
-            target_feature_test_df = target_feature_test_df.replace(TargetValueMapping().to_dict()) # type: ignore
+            target_feature_test_df = target_feature_test_df.replace(TargetValueMapping().to_dict())
 
             preprocessor_object = preprocessor.fit(input_feature_train_df)
             transformed_input_train_feature = preprocessor_object.transform(input_feature_train_df)
-            transformed_input_test_feature = preprocessor_object.transform(input_feature_test_df)
+            transformed_input_test_feature =preprocessor_object.transform(input_feature_test_df)
 
-            smt = SMOTETomek(sampling_strategy="minority")
+            smt = SMOTETomek(sampling_strategy="auto")
 
-            input_feature_train_final, target_feature_train_final = smt.fit_resample(transformed_input_train_feature, target_feature_train_df) # type: ignore
-            input_feature_test_final, target_feature_test_final = smt.fit_resample(transformed_input_test_feature, target_feature_test_df) # type: ignore
+            input_feature_train_final, target_feature_train_final = smt.fit_resample(
+                transformed_input_train_feature, target_feature_train_df
+            )
 
-            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_df)]
-            test_arr = np.c_[input_feature_test_final, np.array(target_feature_test_final)]
+            input_feature_test_final, target_feature_test_final = smt.fit_resample(
+                transformed_input_test_feature, target_feature_test_df
+            )
+
+            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final) ]
+            test_arr = np.c_[ input_feature_test_final, np.array(target_feature_test_final) ]
 
             #save numpy array data
-            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr,)
+            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
             save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
             save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
-
+            
+            
             #preparing artifact
             data_transformation_artifact = DataTransformationArtifact(
-                transformed_object_file_path = self.data_transformation_config.transformed_object_file_path,
-                transformed_train_file_path = self.data_transformation_config.transformed_train_file_path,
-                transformed_test_file_path = self.data_transformation_config.transformed_test_file_path,
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,
             )
             logging.info(f"Data transformation artifact: {data_transformation_artifact}")
             return data_transformation_artifact
